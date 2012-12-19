ea5bf302c7ec5cec5eb9e1dc33529e0bfc9e940d
shomah4a <shoma@nagatsuki.(none)>
2012-12-19 23:09:08 +0900

    init

---
+++ sakuya/__init__.py
@@ -0,0 +1,6 @@
+#-*- coding:utf-8 -*-
+
+__version__ = '0.1.0'
+__author__ = '@shomah4a'
+__license__ = 'GPLv3'
+
---
+++ setup.py
@@ -0,0 +1,34 @@
+#-*- coding:utf-8 -*-
+
+import setuptools
+import sakuya as pkg
+
+pkgname = pkg.__name__
+
+setuptools.setup(
+    name=pkgname,
+    version=pkg.__version__,
+    packages=[pkgname],
+    install_requires=[
+        'sqlalchemy',
+        'pygments',
+        ],
+    author=pkg.__author__,
+    license=pkg.__license__,
+    url='https://github.com/shomah4a/implicit',
+    description='This module provides scala implicit conversion and implicit parameter mechanism for python.',
+    long_description=pkg.__doc__,
+    entry_points=dict(
+        console_scripts=['sakuya=sakuya:main'],
+        ),
+    classifiers='''
+Programming Language :: Python
+Development Status :: 2 - Pre-Alpha
+License :: OSI Approved :: GNU General Public License v3 (GPLv3)
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
+Topic :: Software Development :: Libraries :: Python Modules
+Topic :: Utilities
+'''.strip().splitlines())
+
